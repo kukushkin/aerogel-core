3cec29b1739a9862e85ab721924eb96a5c16c94b
Alex Kukushkin <alex@kukushk.in>
2014-02-21 16:19:29 +0200

    Minor refactoring #icon

--- app/helpers/icon.rb
+++ app/helpers/icon.rb
@@ -1,6 +1,4 @@
 def icon( name, opts = {} )
   icon_family = name.split('-').first
-  opts[:class] = "#{icon_family} #{name}"
-  opts[:style] ||= "vertical-align: middle"
-  tag :i, "", opts
+  tag :i, "", opts.merge( class: "#{icon_family} #{name}", style: "vertical-align: middle" )
 end
