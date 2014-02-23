4c0ee4533324a118ce21edef6bb7c30a7d38c981
Alex Kukushkin <alex@kukushk.in>
2014-02-23 21:29:31 +0200

    Introduced #page_title helper

--- app/helpers/render.rb
+++ app/helpers/render.rb
@@ -1,24 +1,37 @@
-# module Aerogel::Helpers
 
-  # Escapes html string.
-  #
-  def h( str )
-    Rack::Utils.escape_html(str)
-  end
+# Escapes html string.
+#
+def h( str )
+  Rack::Utils.escape_html(str)
+end
 
-  # Renders erb template.
-  #
-  def view( name, *args )
-    erb( "#{name}.html".to_sym, *args )
-  end
+# Renders erb template.
+#
+def view( name, *args )
+  erb( "#{name}.html".to_sym, *args )
+end
 
-  # Renders partial erb template.
-  #
-  def partial( name, opts = {} )
-    name_parts = name.to_s.split('/')
-    name_parts[-1] = '_'+name_parts[-1]+".html"
-    opts[:layout] = false
-    erb name_parts.join('/').to_sym, opts
-  end
+# Renders partial erb template.
+#
+def partial( name, opts = {} )
+  name_parts = name.to_s.split('/')
+  name_parts[-1] = '_'+name_parts[-1]+".html"
+  opts[:layout] = false
+  erb name_parts.join('/').to_sym, opts
+end
 
-# end # module Aerogel::Helpers
+# Sets/gets page title.
+#
+# Example:
+#   page_title "Home page" # sets page title
+#   page_title # => "Home page"
+#
+# Or in views:
+#  <% page_title "Home page" %> # sets page title
+#  <%= page_title %> # gets page title
+#
+def page_title( value = nil )
+  @page_title = value unless value.nil?
+  @page_title
+end
+
