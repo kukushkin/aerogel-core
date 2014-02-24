a720db6f48347751ac0d229d07394e2955def40a
Alex Kukushkin <alex@kukushk.in>
2014-02-24 20:27:11 +0200

    Improved #partial helper
    - now allows rendering of collections

--- app/helpers/render.rb
+++ app/helpers/render.rb
@@ -15,9 +15,25 @@ end
 #
 def partial( name, opts = {} )
   name_parts = name.to_s.split('/')
-  name_parts[-1] = '_'+name_parts[-1]+".html"
+  partial_name = name_parts[-1]
+  name_parts[-1] = '_'+partial_name+".html"
+  template_name = name_parts.join('/').to_sym
   opts[:layout] = false
-  erb name_parts.join('/').to_sym, opts
+
+  # render single template
+  unless opts[:collection].present?
+    return erb template_name, opts
+  end
+
+  # render collection
+  out = ""
+  opts[:locals] ||= {}
+  opts[:collection].each do |object|
+    opts[:locals][partial_name.to_sym] = object
+    out += opts[:delimiter] if opts[:delimiter].present? && out.present?
+    out += erb template_name, opts.except( :collection, :delimiter )
+  end
+  out
 end
 
 # Sets/gets page title.
