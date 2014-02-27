c7998d0e62130034d4424b5eae73e2969b76f379
Alex Kukushkin <alex@kukushk.in>
2014-02-27 20:13:54 +0200

    Fixed #icon helper
    - removed 'vertical-align' style attribute

--- app/helpers/icon.rb
+++ app/helpers/icon.rb
@@ -1,4 +1,5 @@
 def icon( name, opts = {} )
   icon_family = name.split('-').first
-  tag :i, "", opts.merge( class: "#{icon_family} #{name}", style: "vertical-align: middle" )
+  #  tag :i, "", opts.merge( class: "#{icon_family} #{name}", style: "vertical-align: middle" )
+  tag :i, "", opts.merge( class: "#{icon_family} #{name}" )
 end
