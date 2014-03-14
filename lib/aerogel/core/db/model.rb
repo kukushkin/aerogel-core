098f3b9879e6aa6ed8f0b150bcf3e68a3df8e8e1
Alex Kukushkin <alex@kukushk.in>
2014-03-14 21:29:22 +0200

    Modified Model, now allowing custom field definitions based on :type option
    - used in aerogel-media, to allow for 'type: Dragonfly' *magic*

--- lib/aerogel/core/db/model.rb
+++ lib/aerogel/core/db/model.rb
@@ -3,22 +3,54 @@
 module Model
   extend ActiveSupport::Concern
 
-  included do
-    include Mongoid::Document
+  def self.redefined_field_types
+    @redefined_field_types ||= {}
+  end
+
+  def self.define_field_type( type, method_name )
+    redefined_field_types[type] = method_name.to_sym
+  end
+
+  module ClassMethods
 
     # Finds document by id.
     # Raises error if document is not found.
     #
-    def self.find!( *args )
+    def find!( *args )
       raise_not_found_error_was = Mongoid.raise_not_found_error
       begin
         Mongoid.raise_not_found_error = true
-        self.find *args
+        self.find( *args )
       ensure
         Mongoid.raise_not_found_error = raise_not_found_error_was
       end
     end
 
+    # ...
+    # other class methods can be defined inside Model::ClassMethods by aerogel modules
+    # ...
+
+  end # module ClassMethods
+
+  included do
+    include Mongoid::Document
+
+    class << self
+      alias_method :define_field_mongoid, :field
+    end
+
+    # Defines native Mongoid field or calls field type handler
+    # in case of redefined field types.
+    #
+    def self.field( name, opts = {} )
+      if opts[:type] && Model.redefined_field_types.key?( opts[:type] )
+        self.send Model.redefined_field_types[opts[:type]], name, opts
+      else
+        self.define_field_mongoid( name, opts )
+      end
+    end
+
+    extend ClassMethods
   end
 
   module NonPersistent
